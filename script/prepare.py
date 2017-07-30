a6e49378dc00fe033a9a079cdf9c445dfeb1e3a9
yennanliue <f339339@gmail.com>
2017-07-30 10:11:49 +0100

    update feature engineering part

--- script/prepare.py
+++ script/prepare.py
@@ -7,7 +7,7 @@
 import pandas as pd, numpy as np
 import calendar
 
-
+### ================================================ ###
 # basic feature extract 
 
 def basic_feature_extract(df):
@@ -39,22 +39,109 @@ def get_weekday(df):
     df_['pickup_weekday_'] = df_['pickup_week_'].apply(lambda x: calendar.day_name[x])
     return df_
 
+### ================================================ ###
 # feature engineering 
+
+# Haversine distance
 def get_haversine_distance(lat1, lng1, lat2, lng2):
+    # km
     lat1, lng1, lat2, lng2 = map(np.radians, (lat1, lng1, lat2, lng2))
-    AVG_EARTH_RADIUS = 6371  # in km
+    AVG_EARTH_RADIUS = 6371  #  km
     lat = lat2 - lat1
     lng = lng2 - lng1
     d = np.sin(lat * 0.5) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(lng * 0.5) ** 2
     h = 2 * AVG_EARTH_RADIUS * np.arcsin(np.sqrt(d))
     return h 
 
+# Manhattan distance
+# Taxi cant fly ! have to move in blocks/roads
 def get_manhattan_distance(lat1, lng1, lat2, lng2):
+    # km 
     a = get_haversine_distance(lat1, lng1, lat1, lng2)
     b = get_haversine_distance(lat1, lng1, lat2, lng1)
     return a + b
 
+
+# get direction (arc tangent angle)
+def get_direction(lat1, lng1, lat2, lng2):
+    # theta
+    AVG_EARTH_RADIUS = 6371  #  km
+    lng_delta_rad = np.radians(lng2 - lng1)
+    lat1, lng1, lat2, lng2 = map(np.radians, (lat1, lng1, lat2, lng2))
+    y = np.sin(lng_delta_rad) * np.cos(lat2)
+    x = np.cos(lat1) * np.sin(lat2) - np.sin(lat1) * np.cos(lat2) * np.cos(lng_delta_rad)
+    return np.degrees(np.arctan2(y, x))
+
+
+# PCA to transform longitude and latitude
+# to improve decision tree performance 
+from sklearn.decomposition import PCA
+def pca_lon_lat(dftrain,dftest):
+    coords = np.vstack \
+            ((dftrain[['pickup_latitude', 'pickup_longitude']].values,
+              dftrain[['dropoff_latitude', 'dropoff_longitude']].values,
+              dftest[['pickup_latitude', 'pickup_longitude']].values,
+              dftest[['dropoff_latitude', 'dropoff_longitude']].values))
+    pca = PCA().fit(coords)
+    dftrain['pickup_pca0'] = pca.transform(dftrain[['pickup_latitude', 'pickup_longitude']])[:, 0]
+    dftrain['pickup_pca1'] = pca.transform(dftrain[['pickup_latitude', 'pickup_longitude']])[:, 1]
+    dftrain['dropoff_pca0'] = pca.transform(dftrain[['dropoff_latitude', 'dropoff_longitude']])[:, 0]
+    dftrain['dropoff_pca1'] = pca.transform(dftrain[['dropoff_latitude', 'dropoff_longitude']])[:, 1]
+    dftest['pickup_pca0'] = pca.transform(dftest[['pickup_latitude', 'pickup_longitude']])[:, 0]
+    dftest['pickup_pca1'] = pca.transform(dftest[['pickup_latitude', 'pickup_longitude']])[:, 1]
+    dftest['dropoff_pca0'] = pca.transform(dftest[['dropoff_latitude', 'dropoff_longitude']])[:, 0]
+    dftest['dropoff_pca1'] = pca.transform(dftest[['dropoff_latitude', 'dropoff_longitude']])[:, 1]
+    return dftrain,dftest 
+
+
+# get Average speed for pickup location (taxi velocity)
+def avg_speed(df):
+    df_ = df.copy()
+    df_.loc[:, 'pickup_lat_'] = np.round(df_['pickup_latitude'], 3)
+    df_.loc[:, 'pickup_long_'] = np.round(df_['pickup_longitude'], 3)
+    gby_cols = ['pickup_lat_', 'pickup_long_']
+    coord_speed = df_.groupby(gby_cols).mean()[['avg_speed_h']].reset_index()
+    coord_speed.columns = ['pickup_lat_','pickup_long_','avg_area_speed_h']
+    # merge avg area speed and original dataframe
+    df_ = pd.merge(df_, coord_speed,  how='outer')
+    return df_
+
+
+### ======================== ###
+
+def get_features(df):
+    # km 
+    df_ = df.copy()
+    ###  USING .loc making return array ordering 
+    # distance
+    df_.loc[:, 'distance_haversine'] = get_haversine_distance(
+                      df_['pickup_latitude'].values,
+                      df_['pickup_longitude'].values,
+                      df_['dropoff_latitude'].values,
+                      df_['dropoff_longitude'].values)
+    df_.loc[:, 'distance_manhattan'] = get_manhattan_distance(
+                      df_['pickup_latitude'].values,
+                      df_['pickup_longitude'].values,
+                      df_['dropoff_latitude'].values,
+                      df_['dropoff_longitude'].values)
+    # direction 
+    df_.loc[:, 'direction'] = get_direction(df_['pickup_latitude'].values,
+                                          df_['pickup_longitude'].values, 
+                                          df_['dropoff_latitude'].values, 
+                                          df_['dropoff_longitude'].values)
+    # Get Average driving speed 
+    # km/hr
+    # (km/sec = 3600 * (km/hr))
+    df_.loc[:, 'avg_speed_h'] = 3600 * df_['distance_haversine'] / df_['trip_duration']
+    df_.loc[:, 'avg_speed_m'] = 3600 * df_['distance_manhattan'] / df_['trip_duration']
+    
+    return df_
+
+
+
+### ================================================ ###
 # data cleaning
+
 def clean_data(df):
     df_ = df.copy()
     # remove possible outlier in haversine distance 
@@ -65,7 +152,9 @@ def clean_data(df):
          (df_['trip_duration'] > df_['trip_duration'].quantile(0.05))]
     return df_
 
+### ================================================ ###
 # load data
+
 def load_data():
 	df_train = pd.read_csv('~/NYC_Taxi_Trip_Duration/data/train.csv')
 	df_test = pd.read_csv('~/NYC_Taxi_Trip_Duration/data/test.csv')
