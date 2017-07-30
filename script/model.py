d4a9f0bac3eaf03a9a6c3977477d6fa5e2bf719d
yennanliue <f339339@gmail.com>
2017-07-30 11:15:07 +0100

    fix saved model name

--- script/model.py
+++ script/model.py
@@ -2,13 +2,16 @@
 
 # script for model I/O 
 import pickle 
+from datetime import datetime
 
 
+today_ = datetime.now().strftime("%m%d")
+print (today_)
 
 # save model 
 def save_model(model):
     try:
-        with open('../model/model_0726.pkl', 'wb') as fid:
+        with open('../model/model_{}.pkl'.format(today_), 'wb') as fid:
             pickle.dump(model, fid)
             print ('model save success')
     except:
@@ -17,7 +20,13 @@ def save_model(model):
 
 
 # load model 
-def load_model():       
-    with open('../model/model_0726.pkl', 'rb') as fid:        
-        loaded_model = pickle.load(fid)     
-        return loaded_model
+def load_model():
+	try:       
+		#with open('../model/model_0730.pkl', 'rb') as fid:  
+		with open('../model/model_{}.pkl'.format(today_), 'rb') as fid:      
+			loaded_model = pickle.load(fid)     
+			return loaded_model
+	except:
+		print ('model load fail')
+
+
