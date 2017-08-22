8ab2374993c9a3694746006149a859d41d136693
yennanliue <f339339@gmail.com>
2017-08-23 00:53:22 +0100

    revise time relative features function to class method

--- script/prepare.py
+++ script/prepare.py
@@ -2,10 +2,67 @@
 
 # script for data preparation 
 
-
 # basic library 
 import pandas as pd, numpy as np
 import calendar
+
+
+
+
+### ================================================ ###
+# refactor  all feature collect functions to class method (doing)
+
+class feature_datetime:
+
+    def __init__( self, df, column ):
+        self.datetime = pd.to_datetime( df[column] )
+
+    def get_year(self):
+        return self.datetime.dt.year.astype(int)
+
+    def get_month(self):
+        return self.datetime.dt.month.astype(int)
+
+    def get_day(self):
+        return self.datetime.dt.day.astype(int)
+    
+    def get_hour(self):
+        return self.datetime.dt.hour.astype(int)
+    
+    def get_minute(self):
+        return self.datetime.dt.minute.astype(int)
+    
+    def get_second(self):
+        return self.datetime.dt.second.astype(int)
+    
+    def get_daytime(self):
+        return self.get_hour() + self.get_minute()/60. + self.get_second()/60./60.
+    
+    def get_weekday(self):
+        return self.datetime.dt.weekday.astype(int) + 1
+    
+    def get_digit_date(self):
+        return self.get_month()*100 + self.get_day()
+    
+    def get_time_delta(self):
+        return (self.datetime - self.datetime.min()).dt.total_seconds().astype(int)
+    
+    def get_weektime(self):
+        return self.datetime.dt.weekday + (( self.datetime.dt.hour + ( self.datetime.dt.minute/60. ))/24. )
+    
+    def get_weekofyear(self):
+        return self.datetime.dt.weekofyear.astype(int)
+    
+    def get_sin_weektime(self):
+        return np.sin( (self.get_weektime()/7.)*np.pi )**2
+    
+    def get_sin_daytime(self):
+        return np.sin( (self.get_daytime()/24.)*np.pi )**2
+    
+    def get_sin_hour(self):
+        return np.sin( (self.get_hour()/24.)*np.pi )**2
+    
+
 
 ### ================================================ ###
 # basic feature extract 
