243e0ba04ed101526debdf8589c4637809872b82
yennanliue <f339339@gmail.com>
2017-09-26 10:38:46 +0100

    backup spark data prepare script

---
+++ spark_/prepare.py
@@ -0,0 +1,38 @@
+# python 2.7 
+
+
+import os
+
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext
+from pyspark.sql.functions import count, avg
+
+
+sc =SparkContext()
+SparkContext.getOrCreate()
+conf = SparkConf().setAppName("building a warehouse")
+#sc = SparkContext(conf=conf)
+sqlCtx = SQLContext(sc)
+print ("==================")
+print (sc)
+print ("==================")
+
+
+
+
+def run():
+	df_train = sqlCtx.read.format('com.databricks.spark.csv')\
+	                      .options(header='true', inferschema='true')\
+	                      .load('/Users/yennanliu/NYC_Taxi_Trip_Duration/data/train.csv')
+	df_group1= df_train.groupBy("store_and_fwd_flag", "vendor_id")\
+	                   .agg(avg("pickup_longitude"), count("*"))
+	df_group1.show()
+	print (df_group1.show())
+	return df_group1
+
+
+run()
+
+
+
+
