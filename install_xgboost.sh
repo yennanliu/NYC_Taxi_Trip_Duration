8308a0dfd64dfad736ea1022734b70f00c73541b
yennanliue <f339339@gmail.com>
2018-07-22 10:58:32 +0100

    add comments

--- install_xgboost.sh
+++ install_xgboost.sh
@@ -6,7 +6,7 @@
 # install xgboost manually 
 # https://stackoverflow.com/questions/43327020/xgboostlibrarynotfound-cannot-find-xgboost-library-in-the-candidate-path-did-y
 # http://xgboost.readthedocs.io/en/latest/build.html
-
+# will need to install gcc@7 (or gcc@5) before install xgboost
 
 cd ~ 
 git clone --recursive https://github.com/dmlc/xgboost.git
