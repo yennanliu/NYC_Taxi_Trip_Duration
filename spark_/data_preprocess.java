aacee38626074cf909039209955bb9006532a0ad
yennanliue <f339339@gmail.com>
2018-08-14 11:13:55 +0100

    add java code preprocess data

---
+++ spark_/data_preprocess.java
@@ -0,0 +1,50 @@
+
+import java.io.*;
+import java.sql.*;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.when;
+
+public class data_preprocess {
+
+  public static final String delimiter = ",";
+  public static void main(String[] args) {
+  // step 1 : read csv via CSVReader 
+  //String csvFile = "train_data_java.csv";
+  //CSVReader.read(csvFile);
+  // step 2 : read csv via spark 
+  //String args = "some arg";
+  read_csv("test");
+
+}
+
+
+
+  public static void read_csv (String csvFile)
+
+  {
+    SparkSession sparkSession = SparkSession.builder().appName("data_preprocess").config("spark.master", "local").getOrCreate();
+    String PATH = "train_data_java.csv";
+    Dataset<Row> rawData = sparkSession.read().option("header", "true").csv(PATH);
+    Dataset<Row> transformedDataSet = rawData.withColumn("vendor_id", rawData.col("vendor_id").cast("double"))
+    .withColumn("passenger_count", rawData.col("passenger_count").cast("double"))
+    .withColumn("pickup_longitude", rawData.col("pickup_longitude").cast("double"))
+    .withColumn("pickup_latitude", rawData.col("pickup_latitude").cast("double"))
+    .withColumn("dropoff_longitude", rawData.col("dropoff_longitude").cast("double"))
+    .withColumn("dropoff_latitude", rawData.col("dropoff_latitude").cast("double")) 
+    .withColumn("trip_duration", rawData.col("trip_duration").cast("double")); 
+    System.out.println(" ---------------- print csv : ----------------");
+    // print first 20 row of csv 
+    transformedDataSet.show(20);
+    System.out.println(transformedDataSet);
+
+  }
+
+}
+
+
+
