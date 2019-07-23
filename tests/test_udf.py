42bd1ef1fb5b8027086537e5edee6430d1ea63bb
yennanliu <f339339@gmail.com>
2019-07-23 15:16:10 +0800

    add test : test_get_direction  (tests/test_udf.py)

--- tests/test_udf.py
+++ tests/test_udf.py
@@ -1,7 +1,8 @@
 # test module 
 import pytest, unittest
 # import to-test udf (user defined function) 
-from run.train import get_haversine_distance, get_manhattan_distance
+from run.train import get_haversine_distance, get_manhattan_distance,\
+                      get_direction
 
 def test_get_haversine_distance():
     lat1, lng1, lat2, lng2 = -73.98,40.73,-73.99,40.75 
@@ -14,6 +15,10 @@ def test_get_manhattan_distance():
     b = get_haversine_distance(lat1, lng1, lat2, lng1) 
     assert a+b == 1.7256849524059557
 
+def test_get_direction():
+    lat1, lng1, lat2, lng2 = -73.98,40.73,-73.99,40.75 
+    direction = get_direction(lat1, lng1, lat2, lng2)
+    assert direction == 151.1206636530826
 
 if __name__ == '__main__':
     pytest.main([__file__])
